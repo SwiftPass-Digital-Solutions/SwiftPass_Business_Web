1d90e2d07d71932aa9a2a8438de9ce258aebd02a
Kolawole Ayoola <ayoolak@ymail.com>
2026-01-09 23:32:34 +0100

    chore: updated csp headers settings

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ const CSP_DEV =
   "default-src 'self' 'unsafe-inline' http://localhost:5173 swiftpass-backend.onrender.com fonts.googleapis.com; img-src 'self' blob: data: https: https://lh3.googleusercontent.com; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src-elem 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; upgrade-insecure-requests;";
 
 const CSP_PROD =
-  "default-src 'self'; img-src 'self' https:; script-src 'self'; style-src 'self';";
+  "default-src 'self'; img-src 'self' https: data: blob:; script-src 'self'; style-src 'self'; style-src-elem 'self' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; connect-src 'self' swiftpass-backend.onrender.com; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; upgrade-insecure-requests;";
 
 function cspMetaPlugin(csp: string): Plugin {
   return {
